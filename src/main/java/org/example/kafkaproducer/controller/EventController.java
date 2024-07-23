d9feae740d62c65539fb9c654b5e2fb6e321eaca
Carlos D <dario20049@icloud.com>
2024-07-23 19:17:39 +0900

    KafkaMessagePublisher

---
+++ src/main/java/org/example/kafkaproducer/controller/EventController.java
@@ -0,0 +1,4 @@
+package org.example.kafkaproducer.controller;
+
+public class EventController {
+}
---
+++ src/main/java/org/example/kafkaproducer/service/KafkaMessagePublisher.java
@@ -0,0 +1,27 @@
+package org.example.kafkaproducer.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class KafkaMessagePublisher {
+
+    @Autowired
+    private KafkaTemplate<String, Object>  kafkaTemplate;
+
+    public void sendMessageToKafkaTopic(String message) {
+        CompletableFuture<SendResult<String, Object>> send = kafkaTemplate.send("java-testTopic", message);
+        send.whenComplete((result, ex) -> {
+            if (ex != null) {
+                System.out.println("Error while sending message to Kafka Topic");
+            } else {
+                System.out.println("Message sent to Kafka Topic");
+            }
+        });
+    }
+
+}
