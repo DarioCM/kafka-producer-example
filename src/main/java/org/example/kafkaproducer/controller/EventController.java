62372f3f5b2c07ff4cc833b4af339d613e08c9f7
Carlos D <dario20049@icloud.com>
2024-08-06 15:26:18 +0900

    add logger

--- src/main/java/org/example/kafkaproducer/controller/EventController.java
+++ src/main/java/org/example/kafkaproducer/controller/EventController.java
@@ -2,6 +2,8 @@ package org.example.kafkaproducer.controller;
 
 import org.example.dto.Customer;
 import org.example.kafkaproducer.service.KafkaMessagePublisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/producer-app")
 public class EventController {
 
+    private static final Logger logger = LoggerFactory.getLogger(EventController.class);
+
     @Autowired
     private KafkaMessagePublisher kafkaMessagePublisher;
 
@@ -18,15 +22,17 @@ public class EventController {
     public ResponseEntity<?> sendMessageToKafkaTopic(@PathVariable String message) {
         try {
             kafkaMessagePublisher.sendMessageToKafkaTopic(message);
+            logger.info("Message sent to Kafka Topic : {}", message);
             return ResponseEntity.ok("Message sent to Kafka Topic");
         } catch (Exception e) {
+            logger.error("Error while sending message to Kafka Topic : {}", e.getMessage());
             return ResponseEntity.status(500).body("Error while sending message to Kafka Topic");
         }
     }
 
     @PostMapping("/publish")
     public void sendEventsToKafkaTopic(@RequestBody Customer customer) {
-        //System.out.println("Controller : " + customer.getName() + " " + customer.getEmail());
+        logger.info("Customer Controller sent to Kafka Topic : {}", customer.toString());
         kafkaMessagePublisher.sendEventsToKafkaTopic(customer);
     }
 }
